76874a7fc3bc57fee01b74700d801126c04e36a9
Steven <sastudillo6@gmail.com>
2021-03-05 12:32:04 -0500

    Basic card layout for drinks.

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -6,8 +6,25 @@ const Drink = ({ drink }) => {
         <Card>
             <a href={`/drink/${drink._id}`}>
                 <Card.Img src={drink.image} />
-                
             </a>
+
+            <Card.Body>
+                 <a href={`/drink/${drink._id}`}>
+                     <Card.Title as='div'>
+                         <h4>{drink.name}</h4>
+                     </Card.Title>
+                 </a>
+
+            <Card.Text as='div'>
+                <div>
+                    {drink.rating} from {drink.numReviews} reviews
+                </div>
+            </Card.Text>
+                <h4>${drink.price}</h4>
+            <Card.Text>
+
+            </Card.Text>
+            </Card.Body>
         </Card>
     )
 }
