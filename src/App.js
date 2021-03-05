3d6b1541879695c1a1515477baf46f65e261ae07
Steven <sastudillo6@gmail.com>
2021-03-05 17:37:26 -0500

    Testing router, implementing pending architecture for routes.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from "./components/Head"
 import Footer from "./components/Footer"
 import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from "react-bootstrap"
 
@@ -14,6 +15,7 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div>
+            This is the Product Screen Component.
+        </div>
+    )
+};
+
+export default ProductScreen;
