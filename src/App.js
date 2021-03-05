b4d666bdbc18d01448cc778278f205621b3f7114
Steven <sastudillo6@gmail.com>
2021-03-05 01:43:29 -0500

    Testing React BootStrap.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import Header from "./components/Head"
 import Footer from "./components/Footer"
 
+import { Container } from "react-bootstrap"
+
 function App() {
   return (
     <div className="App">
       <Header />
+        <Container>
+          Testing Container Component.
+        </Container>
       <Footer />
     </div>
   );
