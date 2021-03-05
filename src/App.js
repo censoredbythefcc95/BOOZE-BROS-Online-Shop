c16b43443fc6616fa32ab5e0b34fc0c2f13b3861
Steven <sastudillo6@gmail.com>
2021-03-05 01:35:23 -0500

    Header and Footer components tested.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Head"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
