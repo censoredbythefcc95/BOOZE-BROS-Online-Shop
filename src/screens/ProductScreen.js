900c87fc97a21ca3ae7dd6fe445dfb731d7d7e43
Steven <sastudillo6@gmail.com>
2021-03-05 20:16:50 -0500

    Single page application creation.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Rating from "../components/Rating"
 import drinks from "../alcohol"
 import { Link } from "react-router-dom"
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
 
 const ProductScreen = ({ match }) => {
 
@@ -9,10 +10,22 @@ const ProductScreen = ({ match }) => {
     console.log(drink)
     return (
         <div>
-            <Link className="button" to='/'>
+            <Link className="btn btn-dark my-4" to='/'>
                 Back to previous page
             </Link>
-            
+
+            <Row>
+                <Col md={6}>
+                    <Image src={drink.image} alt={drink.name} fluid />
+                </Col>
+                <Col md={2}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h4>{drink.name}</h4>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
         </div>
     )
 };
