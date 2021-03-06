9936c517b24ff1bab2d8fb9f2adcb00fbc574612
Steven <sastudillo6@gmail.com>
2021-03-05 19:54:41 -0500

    Adding routes to single drink pages.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Rating from "../components/Rating"
+import drinks from "../alcohol"
+import { Link } from "react-router-dom"
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+
+    const drink = drinks.find(p => p._id === match.params.id)
+    console.log(drink)
     return (
         <div>
-            This is the Product Screen Component.
+            <Link className="button" to='/'>
+                Back to previous page
+            </Link>
+            
         </div>
     )
 };
