e6644e3eb94a4dda74bff67e988cb760fdb9ddb8
Steven <sastudillo6@gmail.com>
2021-03-09 18:52:43 -0500

    Redux setup with thunk and DevTools middlware. Spread operator for middleware which has array of thunk to pass in.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 const reducer = combineReducers( {} )
 const firstState = {}
+const middleware = [thunk]
 
-const drinkStore = createStore(reducer, firstState)
+const drinkStore = createStore(reducer, firstState, composeWithDevTools(applyMiddleware([...middleware])))
+
+export default drinkStore;
