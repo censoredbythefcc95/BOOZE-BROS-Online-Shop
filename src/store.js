ba6b5dd57c8c6b54ab636419ae604e78ac53ee0a
Steven <sastudillo6@gmail.com>
2021-03-09 18:48:37 -0500

    Configured redux extension tool with Chrome.

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers( {} )
 const firstState = {}
