72a0c3d562100b5dd7b9587885dc10b8bf29eb9f
Steven <sastudillo6@gmail.com>
2021-03-09 18:55:42 -0500

    Added Provider and Redux drinkStore into index.js.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import drinkStore from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider drinkStore={drinkStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
