c0ba0b42739e2089547a07c225f94b214ea459b6
Steven <sastudillo6@gmail.com>
2021-03-10 12:57:48 -0500

    More changes to my redux reducers and state.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -8,9 +8,11 @@ import { listDrinkDetails, listDrinks } from "../actions/drinkActions";
 
 const ProductScreen = ({ match }) => {
     const dispatch = useDispatch();
+    const drinkDetails = useSelector(state => state.drinkDetails)
+    const { loading, error, drink } = drinkDetails
     useEffect(() => {
       dispatch(listDrinkDetails(match.params.id))
-    }, [dispatch]); // dependency array results in useEffect warning. Probably not a big deal for now.
+    }, [dispatch, match]); // dependency array results in useEffect warning. Probably not a big deal for now.
 
         
     return (
@@ -18,63 +20,65 @@ const ProductScreen = ({ match }) => {
             <Link className="btn btn-dark my-4" to='/'>
                 Back to previous page
             </Link>
-
-            <Row>
-                <Col md={6}>
-                    <Image src={drink.image} alt={drink.name} fluid />
-                </Col>
-                <Col md={2}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h4>{drink.name}</h4>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={drink.rating} 
-                             text={`${drink.numReviews} reviews`} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: ${drink.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {drink.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col>
-                    <Card>
-                        <ListGroup variant="flush">
+                {loading ? <h2>Loading drink</h2> : error ? <h2>${error}</h2> : 
+                (  <Row>
+                    <Col md={6}>
+                        <Image src={drink.image} alt={drink.name} fluid />
+                    </Col>
+                    <Col md={2}>
+                        <ListGroup variant='flush'>
                             <ListGroup.Item>
-                            <Row>
-                                <Col>
-                                    <h4>Price: </h4>
-                                </Col>
-                                <Col>
-                                    <h3>${drink.price}</h3>
-                                </Col>
-                            </Row>
-                            </ListGroup.Item>
-
-                            <ListGroup.Item>
-                            <Row>
-                                <Col>
-                                    <h4>Availability: </h4>
-                                </Col>
-                                <Col>
-                                    {drink.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                </Col>
-                            </Row>
+                                <h4>{drink.name}</h4>
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Button className="btn-block"
-                                        type='button'
-                                        disabled={drink.countInStock === 0}>
-                                        ADD TO CART
-                                </Button>
+                                <Rating value={drink.rating} 
+                                 text={`${drink.numReviews} reviews`} />
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Price: ${drink.price}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Description: {drink.description}
                             </ListGroup.Item>
                         </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        <h4>Price: </h4>
+                                    </Col>
+                                    <Col>
+                                        <h3>${drink.price}</h3>
+                                    </Col>
+                                </Row>
+                                </ListGroup.Item>
+    
+                                <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        <h4>Availability: </h4>
+                                    </Col>
+                                    <Col>
+                                        {drink.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Button className="btn-block"
+                                            type='button'
+                                            disabled={drink.countInStock === 0}>
+                                            ADD TO CART
+                                    </Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+                </Row>
+
+                )}
         </div>
     )
 };
