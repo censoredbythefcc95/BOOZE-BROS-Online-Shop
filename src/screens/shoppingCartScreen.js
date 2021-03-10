88f5dec6a00f0a365136a2663e856fb64585a119
Steven <sastudillo6@gmail.com>
2021-03-10 17:17:56 -0500

    Created shopping cart component.

---
+++ src/screens/shoppingCartScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const shoppingCartScreen = () => {
+    return (
+        <>
+            <div>Shopping Cart Screen Component</div>
+        </>
+    )
+}
+
+export default shoppingCartScreen;
