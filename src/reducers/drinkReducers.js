93186a3db7de7e606b45a3c20b068001c9d53f2a
Steven <sastudillo6@gmail.com>
2021-03-09 19:24:04 -0500

    Making a lot more pushes for reducer since redux is very tricky. Switch statement for state loading state added.

--- src/reducers/drinkReducers.js
+++ src/reducers/drinkReducers.js
@@ -1,3 +1,13 @@
 export const drinkReducer = (state = { drinks: [] }, action) => {
-    
+    //switch statement for cases of list requests
+    switch (action.type) {
+        case 'list_request':
+            return { loading: true, drinks: [] } // if loading, run loading: true
+        case 'list_success':
+            return { loading: false, drinks: action.payload } // if done loading, do not load.
+        case 'list_fail':
+            return { loading: false, error: action.payload } // error if reducer fails to load
+            default:
+                return state
+    }
 }
