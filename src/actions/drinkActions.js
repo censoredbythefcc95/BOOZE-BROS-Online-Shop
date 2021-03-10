48908cdfba405fc71141d0454e24ee93f0389da6
Steven <sastudillo6@gmail.com>
2021-03-09 21:33:15 -0500

    Configuring actions in drinkActions for error handling and passing data.

--- src/actions/drinkActions.js
+++ src/actions/drinkActions.js
@@ -1,2 +1,8 @@
 import { LIST_REQUEST, LIST_SUCCESS, LIST_FAIL } from "../constants/drinkConstant";
 
+export const listDrinks = () => {
+    try {
+        dispatch({ type: LIST_REQUEST })
+        
+    }
+}
